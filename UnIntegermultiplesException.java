a230f66c5af93e48bad489f2440bdbbc2a08142a
HAI-Vision <62526131+HAI-Vision@users.noreply.github.com>
2020-06-03 22:50:44 +0800

    自定义异常类（用户存取款的金额不是100的整数倍）
    当用户存取款的金额不是100的整数倍时，则进行相应的异常处理操作

---
+++ UnIntegermultiplesException.java
@@ -0,0 +1,10 @@
+package atm.demo4;
+
+/**
+ * 存取款金额不是100的整数倍的异常处理
+ */
+public class UnIntegermultiplesException extends Exception {
+    public UnIntegermultiplesException() {
+        super();
+    }
+}
