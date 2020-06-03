f246bb8be0815e569cc121aa54afb791a5530595
HAI-Vision <62526131+HAI-Vision@users.noreply.github.com>
2020-06-03 22:52:40 +0800

    自定义异常类（用户新修改的密码不是纯数字）
    当用户新修改的密码包含字符（即新修改的密码不是纯数字）时，则进行相应的异常处理操作

---
+++ UnPureDigitalException.java
@@ -0,0 +1,11 @@
+package atm.demo4;
+
+/**
+ * 修改的银行卡密码不是纯数字的异常处理
+ */
+public class UnPureDigitalException extends Exception {// 自定义异常
+    public UnPureDigitalException() {
+        super();
+    }
+
+}
